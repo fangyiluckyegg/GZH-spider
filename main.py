066b7004c3997fe7e268937d30b1327a83bfdab7
luckyegg <luckyegg@139.com>
2021-05-14 17:33:50 +0800

    X-Update202105141733

--- main.py
+++ main.py
@@ -1,55 +1,52 @@
+"""""""""""""""""""""""""""""""""""""""""""""
+功能说明
+1、梳理本地公众号目标清单
+2、爬取近期文章记录并PUSH给指定用户
+参考资料
+1、python爬取微信公众号:https://www.cnblogs.com/xiao-apple36/p/9447877.html
+2、Python 微信公众号发送消息:https://www.cnblogs.com/supery007/p/8136295.html
+3、微信公众平台测试帐号申请地址：https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+"""""""""""""""""""""""""""""""""""""""""""""
 # -*- coding: utf-8 -*- 
-# python爬取微信公众号:https://www.cnblogs.com/xiao-apple36/p/9447877.html
 from selenium import webdriver
 import time
 import json
 import requests
 import re
 import random
-# 测试
-#微信公众号账号
+
+#微信公众号账号与密码
 user="luckyegg@139.com"
-#公众号密码
 password="fangyi791021"
 #设置要爬取的公众号列表
 #gzlist=['湖州旅游','爱湖州','湖州银泰城','湖州微生活','南太湖优店']
 gzlist=['湖州旅游']
 
-def weChat_login():
 #登录微信公众号，获取登录之后的cookies信息，并保存到本地文本中
+def weChat_login():
     #定义一个空的字典，存放cookies内容
     post={}
-    #用webdriver启动谷歌浏览器
-    print("启动浏览器，打开微信公众号登录界面")
+    #用webdriver启动谷歌浏览器，打开微信公众号登录界面
     driver = webdriver.Chrome(executable_path='D:/WorkSpace/GZH-spider/chromedriver')
-    #打开微信公众号登录页面
     driver.get('https://mp.weixin.qq.com/')
-    
-    #等待5秒钟
     time.sleep(5)
-    print("正在输入微信公众号登录账号和密码......")
-    #清空账号框中的内容
+    
+    #清空微信公众号登录账号和密码框中的内容并自动填入相应内容    
     driver.find_element_by_xpath("./*//input[@name='account']").clear()
-    #自动填入登录用户名
     driver.find_element_by_xpath("./*//input[@name='account']").send_keys(user)
-    #清空密码框中的内容
     driver.find_element_by_xpath("./*//input[@name='password']").clear()
-    #自动填入登录密码
     driver.find_element_by_xpath("./*//input[@name='password']").send_keys(password)
-
+ 
     # 在自动输完密码之后需要手动点一下记住我
     print("请在登录界面点击:记住账号")
     time.sleep(5)
-    #自动点击登录按钮进行登录
-    #driver.find_element_by_xpath("./*//a[@id='loginBt']").click()
+
+    #自动点击登录按钮进行登录，拿手机扫二维码登录！
     driver.find_element_by_xpath("./*//a[@class='btn_login']").click()
-    # 拿手机扫二维码！
-    print("请拿手机扫码二维码登录公众号")
     time.sleep(20)
-    print("登录成功")
+
     #重新载入公众号登录页，登录之后会显示公众号后台首页，从这个返回内容中获取cookies信息
     driver.get('https://mp.weixin.qq.com/')
-    #获取cookies
     cookie_items = driver.get_cookies()
 
     #获取到的cookies是列表形式，将cookies转成json形式并存入本地名为cookie的文本中
@@ -58,12 +55,9 @@ def weChat_login():
     cookie_str = json.dumps(post)
     with open('cookie.txt', 'w+', encoding='utf-8') as f:
         f.write(cookie_str)
-    print("cookies信息已保存到本地")
 
-#爬取微信公众号文章，并存在本地文本中
+#爬取微信公众号query的文章，并存在本地文本中
 def get_content(query):
-    #query为要爬取的公众号名称
-    #公众号主页
     url = 'https://mp.weixin.qq.com'
     #设置headers
     header = {
@@ -76,7 +70,7 @@ def get_content(query):
         cookie = f.read()
     cookies = json.loads(cookie)
 
-    #登录之后的微信公众号首页url变化为：https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=1849751598，从这里获取token信息
+    #登录之后的微信公众号首页url变化到公众号后台首页，从这里获取token信息
     response = requests.get(url=url, cookies=cookies)
     token = re.findall(r'token=(\d+)', str(response.url))[0]
 
@@ -117,6 +111,7 @@ def get_content(query):
         'fakeid': fakeid,
         'type': '9'
         }
+
     #打开搜索的微信公众号文章列表页
     appmsg_response = requests.get(appmsg_url, cookies=cookies, headers=header, params=query_id_data)
     #获取文章总数
@@ -147,24 +142,23 @@ def get_content(query):
         fakeid_list = query_fakeid_response.json().get('app_msg_list')
         #print(fakeid_list)
         for item in fakeid_list:
-            content_date=item.get('create_time')  
-            otherStyleTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(content_date))
+            create_time=item.get('create_time')  
+            content_date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(create_time))
             content_link=item.get('link')
             content_title=item.get('title')
             fileName=query+'.txt'
             with open(fileName,'a',encoding='utf-8') as fh:
                 #fh.write(otherStyleTime+"\n"+content_title+"\n"+content_link+"\n")   
-                m_content = otherStyleTime+"\n"+content_title+"\n"+content_link+"\n"
+                m_content = content_date+"\n"+content_title+"\n"+content_link+"\n"
                 fh.write(m_content)  
-                sendmsg ('olGwn6-YCRvXwHPh4wUlF0LJh2j8',m_content)   
+                #sendmsg ('olGwn6-YCRvXwHPh4wUlF0LJh2j8',m_content)   
+                sendmsg ('olGwn6wvUZTfO4D8QY6hqZPcW5Mo',m_content)   
         num -= 1
         begin = int(begin)
         #begin+=5
         begin+=1
         time.sleep(2)
-    
-#Python 微信公众号发送消息:https://www.cnblogs.com/supery007/p/8136295.html
-#微信公众平台测试帐号申请地址：https://mp.weixin.qq.com/debug/cgi-bin/sandbox?t=sandbox/login
+
 def get_access_token():
     """
     获取微信全局接口的凭证(默认有效期俩个小时)
@@ -209,7 +203,7 @@ def sendmsg(openid,msg):
 if __name__=='__main__':
     try:
         #登录微信公众号，获取登录之后的cookies信息，并保存到本地文本中
-        #weChat_login()
+        weChat_login()
         #登录之后，通过微信公众号后台提供的微信公众号文章接口爬取文章
         for query in gzlist:
             #爬取微信公众号文章，并存在本地文本中
